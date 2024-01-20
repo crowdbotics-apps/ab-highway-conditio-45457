ed192fcd3e5b429e15a417e62051c025f2df05be
Crowdbotics <team@crowdbotics.com>
2024-01-20 07:31:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ab-highway-conditio-45457",
+  url: "https://ab-highway-conditio-45457.botics.co",
+  api: "https://ab-highway-conditio-45457.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
